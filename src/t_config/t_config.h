20bea3eaca248410c38af978d64fa8219c6d3eaf
bitquence <imbitquence@gmail.com>
2025-02-06 18:11:26 +0100

    fix config paramters to be unsigned

--- src/t_config/t_config.h
+++ src/t_config/t_config.h
@@ -19,12 +19,12 @@
 # include <stdint.h>
 
 typedef struct s_config {
-	int32_t		philosopher_count;
-	int32_t		time_to_die;
-	int32_t		time_to_eat;
-	int32_t		time_to_sleep;
-	bool		count_meals;
-	int32_t		min_meals;
+	uint32_t		philosopher_count;
+	uint32_t		time_to_die;
+	uint32_t		time_to_eat;
+	uint32_t		time_to_sleep;
+	bool			count_meals;
+	uint32_t		min_meals;
 }	t_config;
 
 t_error	config_from_args(int argc, char *argv[], t_config *cfg_out);
