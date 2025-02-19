399ede598da527b622ad13abb11a0704cbe7d49a
bitquence <imbitquence@gmail.com>
2025-02-19 03:11:30 +0100

    update t_event to use internal philosopher state

--- src/event/t_event.h
+++ src/event/t_event.h
@@ -3,15 +3,12 @@
 
 # include <stdint.h>
 # include <sys/time.h>
-
-typedef enum e_event_type {
-	STARTED_EATING
-}	t_event_type;
+# include "simulation/philosopher/t_philosopher_state.h"
 
 typedef struct s_event {
-	uint32_t		philosopher_id;
-	struct timeval	timestamp;
-	t_event_type	type;
+	uint32_t			philosopher_id;
+	struct timeval		timestamp;
+	t_philosopher_state	new_state;
 }	t_event;
 
 #endif // T_EVENT_H
---
+++ src/simulation/philosopher/t_philosopher_state.h
@@ -0,0 +1,11 @@
+#ifndef T_PHILOSOPHER_STATE_H
+# define T_PHILOSOPHER_STATE_H
+
+typedef enum s_philosopher_state {
+	THINKING,
+	HOLDING_LFORK,
+	EATING,
+	SLEEPING,
+} t_philosopher_state;
+
+#endif // T_PHILOSOPHER_STATE_H
