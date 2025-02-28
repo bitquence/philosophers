07efceaf70ce28bdb2296f05fe5715d8231e47da
bitquence <imbitquence@gmail.com>
2025-02-28 02:09:12 +0100

    do our best to ensure logs don't go out of order

--- src/simulation/philosopher/transition.c
+++ src/simulation/philosopher/transition.c
@@ -3,6 +3,7 @@
 #include "event/t_event.h"
 #include "event/t_event_node.h"
 #include "event/t_event_log.h"
+#include "time/instant.h"
 
 #include <pthread.h>
 
@@ -11,9 +12,10 @@ t_error	philosopher_transition(t_philosopher *self, t_philosopher_state new)
 	t_event_node	*node;
 
 	self->state = new;
-	node = evl_new(event_new(self->id, new));
+	node = evl_new((t_event){.philosopher_id = self->id, .new_state = new});
 	if (!node)
 		return (E_MALLOC);
+	node->event.timestamp = instant_now();
 	pthread_mutex_lock(self->event_log_mtx);
 	event_log_append_node(self->event_log, node);
 	pthread_mutex_unlock(self->event_log_mtx);
