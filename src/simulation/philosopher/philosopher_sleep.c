8f53ca42bf4f12a5a360a497b22b22aed2f1b68a
bitquence <imbitquence@gmail.com>
2025-03-01 11:52:40 +0100

    ensure sleep does not oversleep

--- src/simulation/philosopher/philosopher_sleep.c
+++ src/simulation/philosopher/philosopher_sleep.c
@@ -11,6 +11,14 @@
 // sleep 5ms before checking if sleep duration has elapsed
 #define SLEEP_INTERVAL_USECONDS 5000
 
+static uint32_t time_until_sleep_ends(t_instant start, t_instant now, uint32_t duration)
+{
+	uint32_t const since_start = duration_since(start, now);
+	if (duration < since_start)
+		return (0);
+	return (duration - since_start);
+}
+
 void	philosopher_sleep(t_philosopher *self, uint32_t ms_duration)
 {
 	struct timeval	start;
@@ -28,6 +36,9 @@ void	philosopher_sleep(t_philosopher *self, uint32_t ms_duration)
 		now = instant_now();
 		if ((uint64_t)duration_since(start, now) > duration)
 			break;
+		uint32_t d = time_until_sleep_ends(start, now, duration);
+		if (SLEEP_INTERVAL_USECONDS > d)
+			return (void)usleep(d);
 		usleep(SLEEP_INTERVAL_USECONDS);
 	}
 }
